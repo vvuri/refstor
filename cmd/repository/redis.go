fe57d496b5801d8702c254c77852c829bc90109a
vvuri <vvuri1978@gmail.com>
2024-08-28 00:25:55 +0300

    fix redis

--- cmd/repository/redis.go
+++ cmd/repository/redis.go
@@ -30,10 +30,23 @@ func (r *RedisRepo) Insert(ctx context.Context, image model.Image) error {
 	sid := fmt.Sprintf("%X", uid[10:])
 	key := imageIDKey(sid)
 
-	res := r.Client.SetNX(ctx, key, string(data), 0)
+	txn := r.Client.TxPipeline()
+
+	res := txn.Client.SetNX(ctx, key, string(data), 0)
 	if err := res.Err(); err != nil {
+		txn.Discard()
 		return fmt.Errorf("Fail to set: %w", err)
 	}
+
+	if err := txn.SAdd(ctx, "images", key).Err(); err != nil {
+		txn.Discard()
+		return fmt.Errorf("Fail to add images key: %w", err)
+	}
+
+	if _, err := txn.Exec(ctx); err != nil {
+		return fmt.Errorf("Fail to exec: %w", err)
+	}
+
 	return nil
 }
 
@@ -59,17 +72,85 @@ func (r *RedisRepo) FindByID(ctx context.Context, sid string) (model.Image, erro
 func (r *RedisRepo) DeleteByID(ctx context.Context, sid string) error {
 	key := imageIDKey(sid)
 
-	err := r.Client.Del(ctx, key).Err()
+	txn := r.Client.TxPipeline()
+
+	err := txn.Client.Del(ctx, key).Err()
 	if erroer.Is(err, redis.Nil) {
+		txn.Discard()
 		return ErrNotExist
 	} else if err != nil {
+		txn.Discard()
 		return fmt.Errorf("Fail to delete by id: %w", err)
+	}
+
+	if err := txn.SRem(ctx, "images", key).Err(); err != nil {
+		txn.Discard()
+		return fmt.Errorf("Fail to add images key: %w", err)
+	}
+
+	if _, err := txn.Exec(ctx); err != nil {
+		return fmt.Errorf("Fail to exec: %w", err)
 	}
 
 	returm nil
 }
 
-func (r *RedisRepo) Update(ctx context.Context, sid string) error {
+func (r *RedisRepo) Update(ctx context.Context, image model.Image) error {
+	data, err := json.Marshal(image)
+	if err != nil {
+		return fmt.Errorf("Fail to encode image: %w", err)
+	}
 
+	key := imageIDKey(data.ImageID)
 
+	res := r.Client.SetNX(ctx, key, string(data), 0)
+	if err := res.Err(); err != nil {
+		return fmt.Errorf("Fail to update: %w", err)
+	}
+
+	return nil
+}
+
+type FindAllPage struct {
+	Size uint64
+	Offset uint64
+}
+
+type FindResult struct {
+	Images []model.Image
+	Cursor uint64
+}
+
+func (r *RedisRepo) FindAll(ctx context.Context, page FindAllPage ) (FindResult, error) {
+	res := r.Client.SScan(ctx, "images", page.Offset, "*", int64(page.Size))
+
+	keys, cursor, err := res.Result()
+	if err != nil {
+		return FindResult{}, fmt.Errorf("Failed to get image ids: %w", err)
+	}
+
+	if len(keys) == 0 {
+		return FindResult{Images: []model.Image}, nil
+	}
+
+	xs, err := r.Client.MGet(ctx, keys...).Result()
+	if err != nil {
+		return FindResult{}, fmt.Errorf("Failed to get images: %w", err)
+	}
+
+	images := make([]model.Image, len(xs))
+
+	for i,x := range xs {
+		x := x.(string)
+		var image model.Image
+
+		err := json.Unmarshal([]byte(x), &image)
+		if err != nil {
+			return FindResult{}, fmt.Errorf("Failed to decode images: %w", err)
+		}
+
+		images[i] = image
+	}
+
+	return FindResult{ Images: images, Cursor: cursor }, nil
 }
