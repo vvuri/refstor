f90d6d88fa92d78ccba78699dd9785eba2ba8f52
vvuri <vvuri1978@gmail.com>
2024-09-15 23:55:31 +0300

    fix create

--- cmd/handler/image.go
+++ cmd/handler/image.go
@@ -1,13 +1,57 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/google/uuid"
 	"net/http"
+	"refstor/cmd/model"
 	"refstor/cmd/repository"
+	"time"
 )
 
 type Image struct {
 	Repo *repository.RedisRepo
+}
+
+func (i *Image) Create(w http.ResponseWriter, r *http.Request) {
+	var body struct {
+		//CustomerID uuid.UUID         `json:"customer_id"`
+		Description string `json:"description"`
+		Link        string `json:"link"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	now := time.Now().UTC()
+
+	image := model.Image{
+		ImageID:     uuid.Nil,
+		Description: body.Description,
+		SmallImg:    nil,
+		Date:        &now,
+		URL:         body.Link,
+	}
+
+	err = i.Repo.Insert(r.Context(), image)
+	if err != nil {
+		fmt.Println("failed to insert record:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	resp, err := json.Marshal(image)
+	if err != nil {
+		fmt.Println("failed to marshal record:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(resp)
+	w.WriteHeader(http.StatusCreated)
 }
 
 func (i *Image) List(w http.ResponseWriter, r *http.Request) {
