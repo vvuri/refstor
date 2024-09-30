4e5be9f56ebddc137cfcf3d9dd36deafc61c37e6
vvuri <vvuri1978@gmail.com>
2024-10-01 00:03:01 +0300

    fix img

--- client/src/components/ImageGallary.vue
+++ client/src/components/ImageGallary.vue
@@ -5,7 +5,27 @@
         cols="3"
     >
       <v-card>
-       Img-{{n}}
+        <v-img
+            :src="`https://i.pravatar.cc/400?img=${n * 5 + 10}`"
+            aspect-ratio="1"
+            class="bg-grey-lighten-2"
+            cover
+        >
+          <template v-slot:placeholder>
+            <v-row
+                align="center"
+                class="fill-height ma-0"
+                justify="center"
+            >
+              <v-progress-circular
+                  color="grey-lighten-5"
+                  size="50"
+                  width="2"
+                  indeterminate
+              ></v-progress-circular>
+            </v-row>
+          </template>
+        </v-img>
       </v-card>
 
     </v-col>
